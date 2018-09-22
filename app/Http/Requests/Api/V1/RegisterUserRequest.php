4388251d91d417206180ab5a10bcdcb09e8f3d31
Duy Ho N <hongocduy91@gmail.com>
2018-09-22 21:49:49 +0700

    feature change field register api

--- app/Http/Requests/Api/V1/RegisterUserRequest.php
+++ app/Http/Requests/Api/V1/RegisterUserRequest.php
@@ -26,14 +26,12 @@ class RegisterUserRequest extends Request
     {
         $rules = [
             'email' => 'required|string|email|unique:users,email',
-            'name' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
             'dob' => 'required|date',
             'gender' => 'required',
             'picture' => 'required|image|mimes:jpg,jpeg,png,gif',
             'cover' => 'required',
-            'zipcode' => 'required',
-            'city' => 'required',
-            'country_id' => 'required',
             'password' => 'required|string|min:6',
             'confirm_password' => 'required|string|same:password'
         ];
