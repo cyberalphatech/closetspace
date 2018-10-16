a3892263ca8d482022c279a9134f48db5447a6e6
Duy Ho N <hongocduy91@gmail.com>
2018-10-16 20:32:25 +0700

    update logout api

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -87,8 +87,7 @@ class AuthController extends Controller
      *   path="/api/v1/me/logout",
      *   tags={"User"},
      *   description="User logout.",
-     *   @OA\Response(response="200", description="An example resource"),
-     *   @OA\Response(response=422, description="Invalid data", @OA\JsonContent(ref="#/components/schemas/ErrorDataReponse")),
+     *   @OA\Response(response="401", description="Logout success!"),
      *   @OA\Response(response=400, description="Bad request", @OA\JsonContent(ref="#/components/schemas/ErrorReponse"))
      * )
      */
